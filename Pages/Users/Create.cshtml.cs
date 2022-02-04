3db36af2ba469ce021e931e67ad6dbc4ad5da3eb
KerenLopez <kerenlopezcordoba@outlook.es>
2022-02-04 17:12:47 -0500

    Changes in sign up user window

---
+++ Pages/Users/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using registerModuleRPC.Data;
+using registerModuleRPC.model;
+
+namespace registerModuleRPC.Pages.Users
+{
+    public class CreateModel : PageModel
+    {
+        private readonly registerModuleRPC.Data.registerModuleRPCContext _context;
+
+        public CreateModel(registerModuleRPC.Data.registerModuleRPCContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public User User { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.User.Add(User);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace registerModuleRPC.model
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDate { get; set; }
+    }
+}
