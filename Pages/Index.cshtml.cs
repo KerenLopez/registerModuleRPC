e615bd08ec6a932b644e2cca6fd0de53e86f201d
AngelicaCorrales <angelicacorrales2002@gmail.com>
2022-02-06 00:36:42 -0500

    Index modified

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -9,11 +9,7 @@ namespace registerModuleRPC.Pages
         private readonly ILogger<IndexModel> _logger;
         private readonly registerModuleRPC.Data.registerModuleRPCContext2 _context;
 
-        //  [BindProperty]
-        //  public string Username { get; set; }
-
-        //[BindProperty]
-        // public string Password { get; set; }
+       
         [BindProperty]
         public User UserRPC { get; set; }
 
@@ -31,53 +27,26 @@ namespace registerModuleRPC.Pages
         
         public void OnPost()
         {
-            //Username= Request.Form["username"];
-         
-           /*
-            if (Username==null)
+
+            var dbUser = _context.User.FirstOrDefault(u => u.UserName == UserRPC.UserName && u.Password == UserRPC.Password);
+            if (dbUser == null)
             {
-                return Redirect("/Users/Create");
+                Page();
+
             }
             else
             {
-
-
-                var dbUser = _context.User.FirstOrDefault(u => u.UserName == Username);
-                if (dbUser == null)
-                {
-                    return Page();
-
-                }
-                else
-                {
-                    return Redirect("/Users/Index");
-
-                }
+                Response.Redirect("/Users/Index");
 
             }
-           */
-            /*
-            List<model.User> users = _context.User.ToList();
-            Boolean exit = false;
-          
-
-            for (int i = 0; i < users.Count() && !exit; i++)
-            {
-                if (users.ElementAt(i).UserName.Equals(Username) && users.ElementAt(i).Password.Equals(Password))
-                {
-                    exit = true;
-                    Response.Redirect("/Users/Index");
-
-                }
-            }
-            */
         }
+    }
            
 
 
-    }
+ }
 
-}
+
         
 
 
